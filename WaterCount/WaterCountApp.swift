2d443d9dd56c79bcf10e023184611e7e9077e5fc
BlueOneThree <cdy69kb9vh@privaterelay.appleid.com>
2026-01-09 19:08:46 +0100

    Initial Commit

---
+++ WaterCount/WaterCountApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterCountApp.swift
+//  WaterCount
+//
+//  Created by FUVE on 09/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct WaterCountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
