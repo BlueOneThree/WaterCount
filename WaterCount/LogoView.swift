e903e99643ad6de15700d2ba5de5c135ec1a2dce
BlueOneThree <cdy69kb9vh@privaterelay.appleid.com>
2026-01-11 19:32:52 +0100

    Add LogoView component for displaying title

--- WaterCount/LogoView.swift
+++ WaterCount/LogoView.swift
@@ -1,1 +1,29 @@
+//
+//  LogoView.swift
+//  WaterCount
+//
+//  Created by FUVE on 09/01/2026.
+//
 
+import SwiftUI
+
+struct LogoView: View {
+    var body: some View {
+        ZStack {
+            Text("Water Counter")
+                .font(.system(.title, design: .rounded))
+                .fontWeight(.heavy)
+                .foregroundStyle(Color.colorThree)
+            Text("Water Counter")
+                .font(.system(.title, design: .rounded))
+                .fontWeight(.heavy)
+                .foregroundStyle(Color.colorFour)
+                .scaleEffect(y: -1)
+                .padding(.top, 55)
+        }
+    }
+}
+
+#Preview {
+    LogoView()
+}
